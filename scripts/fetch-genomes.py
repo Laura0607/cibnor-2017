07e734bda5b98caa9b166993a1fdc65027321d3a
root <root@ip-172-31-22-126.us-west-2.compute.internal>
2017-03-20 19:38:11 +0000

    fixed gb fasta

--- scripts/fetch-genomes.py
+++ scripts/fetch-genomes.py
@@ -6,10 +6,10 @@ import sys
 import time
 
 if len(sys.argv) != 3:
-    print "USAGE: fetch_genome.py <genome_id_list> <out_dir>"
+    print "USAGE: fetch-genomes.py <genome_id_list> <out_dir>"
     sys.exit(1)
 
-url_template = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=%s&rettype=gb&retmode=text"
+url_template = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=%s&rettype=fasta&retmode=text"
 
 os.mkdir(sys.argv[2])
 
@@ -20,11 +20,11 @@ for id in open(sys.argv[1]):
 
     sys.stdout.write("Fetching %s..." % id)
     sys.stdout.flush()
-    gbk_out_file = os.path.join(sys.argv[2], id + ".gbk")
-    if os.path.exists(gbk_out_file):
+    out_file = os.path.join(sys.argv[2], id + ".fa")
+    if os.path.exists(out_file):
         print "already fetched"
 
-    open(gbk_out_file, "w").write(urllib2.urlopen(url_template % id).read())
+    open(out_file, "w").write(urllib2.urlopen(url_template % id).read())
     print "Done"
     time.sleep(1.0/3)
 
